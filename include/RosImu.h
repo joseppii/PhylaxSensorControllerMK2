9480abc7aadb61bd09e7a28031e14cb215a5cdf1
joseppii <i.piperakis@gmail.com>
2022-04-23 22:08:41 +0300

    Convert the RosImu class in an abstract class
    Add two virtual methods startImu and readData
    Remove all references to concrete IMU implementations
    Move the begin method to the concrete implementation
    Move all the IMU setup code from the init to the concrete implementation
    Combine the update and publish methods

--- include/RosImu.h
+++ include/RosImu.h
@@ -1,20 +1,33 @@
 #ifndef ROSIMU_H
 #define ROSIMY_H
 
-#include "ICM_20948.h"
+#include "Wire.h"
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
 #include <sensor_msgs/msg/imu.h>
 
 class RosImu 
 {
 public:
-    RosImu(ICM_20948_I2C& myICM);
-    void begin(TwoWire& i2c, uint8_t ad0);
-    void init();
-    void read();
+    RosImu();
+    ~RosImu();
+    rcl_ret_t init(rcl_node_t& node, const char* topic_name);
+    rcl_ret_t publish(struct timespec time_stamp);
+
+protected:
+    virtual bool startImu(TwoWire &i2c, uint8_t ad0) = 0;
+    virtual void readData() = 0;
 
 private:
-    ICM_20948_I2C* const myICM_; //Create an ICM_20948_I2C object
-    sensor_msgs__msg__Imu imu_msg_;
+    rcl_publisher_t imu_publisher_;   
+    rcl_node_t* node_;
+    sensor_msgs__msg__Imu* imu_msg_;
+
+    const float g_to_accel_ = 9.81;
+    const float mgauss_to_utesla_ = 0.1;
+    float accel_covariance_ = 0.00001;
+    float gyro_covariance_ = 0.00001;
 };
 
 #endif /* ROSIMU_H */
