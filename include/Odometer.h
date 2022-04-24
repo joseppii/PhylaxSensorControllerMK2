c86bc983a3243e54ba2f8d73e9130a4ea56684f5
joseppii <i.piperakis@gmail.com>
2022-04-24 03:02:25 +0300

    Use micro ros to add an odom publisher
    Use micro ros to and an odom message and corresponging publisher
    Setup the message in the constructor
    Implement the destructor to despose publisher and msg
    Modify the init method to accept a node object and a topic name and setup the p>
    Implement publish_odom method

--- include/Odometer.h
+++ include/Odometer.h
@@ -1,24 +1,33 @@
 #ifndef ODOMETRY_H
 #define ODOMETRY_H
 
-#include <Arduino.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <geometry_msgs/msg/transform_stamped.h>
+#include <nav_msgs/msg/odometry.h>
 
 class PololuEncoder;
 
 class Odometer
 {
 public:
-    Odometer(PololuEncoder& encoder1, PololuEncoder& encoder2);
-    void init(float track);
-    void updateEncoder();
+    Odometer(PololuEncoder& encoderLeft, PololuEncoder& encoderRight);
+    ~Odometer();
+    rcl_ret_t init(rcl_node_t& node, const char* topic_name, float track);
+    void updateEncoders();
     void evaluateRobotPose(unsigned long diff_time);
+    rcl_ret_t publish_odom(struct timespec time_stamp);
 
 private:
-
-    PololuEncoder* const encoder1_;
-    PololuEncoder* const encoder2_;
-    uint32_t encoder1CountPrev_;
-    uint32_t encoder2CountPrev_;
+    rcl_publisher_t odom_publisher_;   
+    rcl_node_t* node_;
+    nav_msgs__msg__Odometry* odom_msg_;
+    
+    PololuEncoder* const encoderLeft_;
+    PololuEncoder* const encoderRight_;
+    uint32_t encoderLeftCountPrev_;
+    uint32_t encoderRightCountPrev_;
 
     float track_; 
     float dPhiL_;
--- src/Odometer.cpp
+++ src/Odometer.cpp
@@ -1,45 +1,81 @@
 #include "Odometer.h"
 #include "encoder.h"
 
-Odometer::Odometer(PololuEncoder& encoder1, PololuEncoder& encoder2) : encoder1_(&encoder1), encoder2_(&encoder2)
+Odometer::Odometer(PololuEncoder& encoderLeft, PololuEncoder& encoderRight) : encoderLeft_(&encoderLeft), encoderRight_(&encoderRight)
 {
+  odom_msg_ = nav_msgs__msg__Odometry__create();
+  odom_msg_->header.frame_id.data = (char*)malloc(100*sizeof(char));
+  char string1[] = "odom";
+  memcpy(odom_msg_->header.frame_id.data, string1, strlen(string1) + 1);
+  odom_msg_->header.frame_id.size = strlen(odom_msg_->header.frame_id.data);
+  odom_msg_->header.frame_id.capacity = 100;    
+
+  char string2[] = "base_link";
+  odom_msg_->child_frame_id.data = (char*)malloc(100*sizeof(char));
+  memcpy(odom_msg_->child_frame_id.data, string2, strlen(string2) + 1);
+  odom_msg_->child_frame_id.size = strlen(odom_msg_->child_frame_id.data);
+  odom_msg_->child_frame_id.capacity = 100;    
 
 }
 
-void Odometer::init(/*ros::NodeHandle &nh,*/ float track)
+Odometer::~Odometer()
 {
+  nav_msgs__msg__Odometry__destroy(odom_msg_);
+  rcl_ret_t ret = rcl_publisher_fini(&odom_publisher_, node_);
+}
+
+rcl_ret_t Odometer::init(rcl_node_t& node, const char* topic_name, float track)
+{
+  rcl_ret_t ret;
+  node_ = &node;
   track_ = track;
+
+  if (node_ == nullptr)
+    return RCL_RET_NODE_INVALID;
+
+  //Initialize pololu encoder(s)
+  encoderLeft_->init();
+  encoderRight_->init();
+
+  // create Odom publisher
+  ret = rclc_publisher_init_default( 
+      &odom_publisher_, 
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry),
+      topic_name
+  );
+    return ret;
 }
 
-void Odometer::updateEncoder()
+void Odometer::updateEncoders()
 {
-  uint32_t encoder1Count;
-  uint32_t encoder2Count;
+  uint32_t encoderLeftCount;
+  uint32_t encoderRightCount;
 
-  encoder1_->update(encoder1Count);
-  encoder2_->update(encoder2Count);
+  encoderLeft_->update(encoderLeftCount);
+  encoderRight_->update(encoderRightCount);
 
-  int32_t dEncoder1 = (encoder1Count - encoder1CountPrev_);
-  int32_t dEncoder2 = (encoder2Count - encoder2CountPrev_);
+  int32_t dEncoderLeft = (encoderLeftCount - encoderLeftCountPrev_);
+  int32_t dEncoderRight = (encoderRightCount - encoderRightCountPrev_);
 
   //update the angle increment in radians
-  float dphi1 = ((float)dEncoder1 * encoder1_->_enc2rad);
-  float dphi2 = ((float)dEncoder2 * encoder2_->_enc2rad);
+  float dphi1 = ((float)dEncoderLeft * encoderLeft_->_enc2rad);
+  float dphi2 = ((float)dEncoderRight * encoderRight_->_enc2rad);
   
   //for encoder index and motor position switching (Right is 1, Left is 2)
   dPhiR_ = dphi1;
   dPhiL_ = dphi2;
   
-  encoder1CountPrev_ = encoder1Count;
-  encoder2CountPrev_ = encoder2Count;
+  encoderLeftCountPrev_  = encoderLeftCount;
+  encoderRightCountPrev_ = encoderRightCount;
 }
 
 void Odometer::evaluateRobotPose(unsigned long diff_time)
 {
-  float dTh = encoder1_->_wheelRadius/(track_) *(dPhiR_ - dPhiL_);
-  float dist = encoder1_->_wheelRadius *(dPhiR_ + dPhiL_) / 2;
-  float dx = encoder1_->_wheelRadius/2 * (cos(th_)*dPhiR_ + cos(th_)*dPhiL_);
-  float dy = encoder1_->_wheelRadius/2 * (sin(th_)*dPhiR_+ + sin(th_)*dPhiL_);
+  float dTh = encoderLeft_->_wheelRadius/(track_) *(dPhiR_ - dPhiL_);
+  float dist = encoderLeft_->_wheelRadius *(dPhiR_ + dPhiL_) / 2;
+  float dx = encoderLeft_->_wheelRadius/2 * (cos(th_)*dPhiR_ + cos(th_)*dPhiL_);
+  float dy = encoderLeft_->_wheelRadius/2 * (sin(th_)*dPhiR_+ + sin(th_)*dPhiL_);
   long dt = float(diff_time)/1000;
   
   th_+= dTh;
@@ -49,5 +85,30 @@ void Odometer::evaluateRobotPose(unsigned long diff_time)
   vTh_ = dTh/dt;
   pathDistance_ = pathDistance_ + sqrt(dx*dx + dy*dy);
 
-  Serial.println("Math stuff = "+String((int32_t)pathDistance_));
+  Serial.println("Path distance = "+String((int32_t)pathDistance_));
+}
+ 
+rcl_ret_t Odometer::publish_odom(struct timespec time_stamp) 
+{
+    
+  odom_msg_->pose.pose.position.x  = x_;
+  odom_msg_->pose.pose.position.y  = y_;
+  odom_msg_->pose.pose.position.z  = 0.0;
+ // odom_msg_->pose.pose.orientation = tf::createQuaternionFromYaw(_th);
+
+  odom_msg_->twist.twist.linear.x  = vx_;
+  odom_msg_->twist.twist.linear.y  = 0;
+
+  odom_msg_->twist.twist.angular.x = 0.0;
+  odom_msg_->twist.twist.angular.y = 0.0;  
+  odom_msg_->twist.twist.angular.z = vTh_; 
+
+  odom_msg_->twist.covariance[0]  = 0.0001;
+  odom_msg_->twist.covariance[7]  = 0.0001;
+  odom_msg_->twist.covariance[35] = 0.0001;
+
+  odom_msg_->header.stamp.sec = time_stamp.tv_sec;
+  odom_msg_->header.stamp.nanosec = time_stamp.tv_nsec;
+  return rcl_publish(&odom_publisher_, odom_msg_, NULL);
+
 }
